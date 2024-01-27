f1184ae0a64eae9ca4147a3ed1bba172401e6f66
LAMENTIN28 <perpendomobile@gmail.com>
2024-01-27 14:41:03 +0300

    pre 2.0 fix number 4

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -1,5 +1,6 @@
 from random import randint
 from brain_games.engine.engine import check
+from operator import sub, add, mul
 
 
 def calc(name):
@@ -8,16 +9,11 @@ def calc(name):
     while ind <= 2:
         num1 = randint(1, 100)
         num2 = randint(1, 100)
-        sign = randint(1, 3)
-        if sign == 1:
-            print("Question: " + str(num1) + " + " + str(num2))
-            right_ans = num1 + num2
-        elif sign == 2:
-            print("Question: " + str(num1) + " - " + str(num2))
-            right_ans = num1 - num2
-        elif sign == 3:
-            print("Question: " + str(num1) + " * " + str(num2))
-            right_ans = num1 * num2
+        sign = randint(0, 2)
+        operators_sym = ['+', '-', '*']
+        operators = [add, sub, mul]
+        print("Question: " + str(num1) + ' ' + operators_sym[sign] + ' ' + str(num2))
+        right_ans = operators[sign](num1, num2)
         print("Your answer is: ")
         ans_user = input()
         ind = check(ans_user, right_ans, ind, name)
